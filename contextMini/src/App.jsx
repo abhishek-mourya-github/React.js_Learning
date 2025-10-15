2e09b9886b56996c486eff156db8820d7dc6dded
abhishek_mourya <kartikmourya1947@gmail.com>
2025-10-15 23:00:49 +0530

    pushing the context folder

---
+++ contextMini/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider';
+import Loggin from './components/Loggin';
+import Profile from './components/Profile';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>Hello, Abhishek Mourya</h1>
+      <Loggin />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ contextMini/src/components/Loggin.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useContext} from 'react';
+import UserContext from '../context/UserContext';
+
+function Loggin(){
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const {setUser} = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+         e.preventDefault();
+         setUser({username, password})   
+    }
+    return(
+        <>
+          <h2>Login</h2>
+          <input type="text" 
+          className='border-b-white border-1 mr-2 p-1'
+           value={username}
+           onChange={(e) => setUsername(e.target.value) }
+           placeholder='username'  />
+
+          <input type="text" 
+           className='border-b-white border-1 mr-2 p-1'
+           value={password}
+           onChange={(e) => setPassword(e.target.value) }
+           placeholder='password'  />
+
+          <button className='cursor-pointer border-amber-400 border-2' onClick={handleSubmit}>Submit</button>
+        </>
+    )
+}
+
+export default Loggin;
