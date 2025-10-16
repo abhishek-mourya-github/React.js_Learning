18a03872116c8f6e76014c61474812fc3d8237f1
abhishek_mourya <kartikmourya1947@gmail.com>
2025-10-17 01:09:34 +0530

    adding the themeSwitcher project initial stage code

---
+++ themeSwitcher/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1 className='text-2xl bg-green-200'>Hello, Abhishek Mourya lets start building theme switcher</h1>
+    </>
+  )
+}
+
+export default App
